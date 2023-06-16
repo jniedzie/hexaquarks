888f14d4ad588eb4b439ac7e624778c9b12a30fe
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-06-16 15:07:02 +0200

    Adding input files for the pythia program and updating the program to run correctly

--- pythia_files/hexaquarks_pythia_program.cc
+++ pythia_files/hexaquarks_pythia_program.cc
@@ -24,54 +24,62 @@ bool is_file_ok(char* path){
 }
 
 int main(int argc, char* argv[]) {
-    if(!are_args_ok(argc)) return 1;
-    if(!is_file_ok(argv[1])) return 1;
+  if(!are_args_ok(argc)) return 1;
+  if(!is_file_ok(argv[1])) return 1;
 
-    cout << "\n >>> PYTHIA settings will be read from file " << argv[1]
-      << " <<< \n >>> HepMC events will be written to file "
-      << argv[2] << " <<< \n" << endl;
-    
-    // Prepare Pythia generator
-    Pythia pythia;
-    pythia.readFile(argv[1]);
-    pythia.readString("Print:quiet = on");
-    pythia.readString("HardQCD:hardbbbar = on");
-    int nEvent = pythia.mode("Main:numberOfEvents");
-    int nAbort = pythia.mode("Main:timesAllowErrors");
-    pythia.init();
+  cout << "\n >>> PYTHIA settings will be read from file " << argv[1]
+    << " <<< \n >>> HepMC events will be written to file "
+    << argv[2] << " <<< \n" << endl;
+  
+  // Prepare Pythia generator
+  Pythia pythia;
+  pythia.readFile(argv[1]);
+  pythia.readString("Print:quiet = on");
+  pythia.readString("HardQCD:hardbbbar = on");
+  int nEvent = pythia.mode("Main:numberOfEvents");
+  int nAbort = pythia.mode("Main:timesAllowErrors");
+  pythia.init();
 
-    // Prepare EvtGen decayer
-    EvtGenDecays *evtgen = 0;
-    setenv("PYTHIA8DATA", argv[5], 1);
-    evtgen = new EvtGenDecays(&pythia, argv[3], argv[4]);
-    evtgen->readDecayFile("hexaquarks.dec");
-    
-    Pythia8ToHepMC toHepMC(argv[2]);
+  // Prepare EvtGen decayer
+  EvtGenDecays *evtgen = 0;
+  setenv("PYTHIA8DATA", argv[5], 1);
+  evtgen = new EvtGenDecays(&pythia, argv[3], argv[4]);
+  
+  Pythia8ToHepMC toHepMC(argv[2]);
 
-    // Begin event loop.
-    int iAbort = 0;
-    for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
+  // Begin event loop.
+  int iAbort = 0;
 
-      if (!pythia.next()) {
-        // If failure because reached end of file then exit event loop.
-        if (pythia.info.atEndOfFile()) {
-          cout << " Aborted since reached end of Les Houches Event File\n";
-          break;
-        }
+  cout<<"Will generate "<<nEvent<<" events"<<endl;
 
-        // First few failures write off as "acceptable" errors, then quit.
-        if (++iAbort < nAbort) continue;
-        
-        cout << " Event generation aborted prematurely, owing to error!\n";
+  for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
+    cout<<"Event "<<iEvent<<endl;
+    if (!pythia.next()) {
+      cout<<"There was some failure"<<endl;
+      // If failure because reached end of file then exit event loop.
+      if (pythia.info.atEndOfFile()) {
+        cout << " Aborted since reached end of Les Houches Event File\n";
         break;
       }
 
-    // Perform the decays with EvtGen.
-    if (evtgen) evtgen->decay();
+      // First few failures write off as "acceptable" errors, then quit.
+      if (++iAbort < nAbort) continue;
+      
+      cout << " Event generation aborted prematurely, owing to error!\n";
+      break;
+    }
 
-    
-    toHepMC.writeNextEvent( pythia );
+    // Perform the decays with EvtGen.
+    cout<<"Decaying with EvtGen"<<endl;
+    if (evtgen) evtgen->decay();
+    cout<<"Saving to HepMC"<<endl;
+    try{ toHepMC.writeNextEvent(pythia); }
+    catch(std::out_of_range&){ cout<<"vector out of range"<<endl; }
+    cout<<"Event finished"<<endl;
   }
+  
+
+  cout<<"Events loop finished"<<endl;
   pythia.stat();
 
   if (evtgen) delete evtgen;
