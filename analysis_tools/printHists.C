3d14fad1e68773f969cbdfd196c087a7f107a22d
Szymon Slawinski <szymon.slawinski@desy.de>
2023-08-02 16:54:03 +0200

    some improvements in printHists.C

--- analysis_tools/printHists.C
+++ analysis_tools/printHists.C
@@ -1,3 +1,8 @@
+#include <string>
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TKey.h"
 
 
 void printHists()
@@ -7,10 +12,14 @@ void printHists()
 
     TCanvas* c = new TCanvas("", "", 1200, 1200);
 
+    c->SetLeftMargin(0.15);
+
     for (TObject* keyAsObj : *file->GetListOfKeys())
     {
         TKey* key = (TKey*)keyAsObj;
         TH1D* h = (TH1D*)file->Get(key->GetName());
+
+        h->GetYaxis()->SetTitleOffset(0.2);
 
         c->Clear();
         h->Draw();
